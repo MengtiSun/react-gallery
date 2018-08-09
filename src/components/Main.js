33e7375f87c3af7a7243fe04ce01fe357226468c
Mengti Sun <mengtisun@gmail.com>
2018-08-09 23:14:58 +1000

    wip reverse an image

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,18 +17,38 @@ function getRangeRandom(low, high) {
   return Math.floor(Math.random() * (high - low) + low);
 }
 
+// get a degree between -30 to 30
+function get30DegRandom() {
+  return (Math.random() > 0.5? '' : '-') + Math.floor(Math.random() * 30);
+}
+
 let ImgFigure = React.createClass({
+  handleClick(e) {
+    this.props.inverse();
+    e.stopPropagation();
+    e.preventDefault();
+  },
   render: function() {
     let styleObj = {};
     if (this.props.arrange.pos) {
       styleObj = this.props.arrange.pos;
     }
+    if (this.props.arrange.rotate) {
+      ['-moz-', '-ms-', '-webkit-', ''].forEach((value)=>{
+          styleObj[value + 'transform'] = `rotate(${this.props.arrange.rotate}deg)`;
+      })
+    }
+    let imgFigureClassName = 'img-figure';
+    imgFigureClassName += this.props.arrange.isInverse? ' is-inverse': '';
     return (
-      <figure className="img-figure" style={styleObj}>
+      <figure className={imgFigureClassName} style={styleObj} onClick={this.handleClick}>
         <img src={this.props.data.imageUrl}
              alt={this.props.data.title}/>
         <figcaption>
           <h2 className="img-title">{this.props.data.title}</h2>
+          <div className="img-back" onClick={this.handleClick}>
+            <p>{this.props.data.desc}</p>
+          </div>
         </figcaption>
       </figure>
     );
@@ -63,6 +83,17 @@ class AppComponent extends React.Component {
       ]
     };
   }
+  // inverse an image
+  inverse(index) {
+    return ()=> {
+      let imgsArrangeArr = this.state.imgsArrangeArr;
+      imgsArrangeArr[index].isInverse = !imgsArrangeArr[index].isInverse;
+      this.setState({
+        imgsArrangeArr: imgsArrangeArr
+      })
+    }
+  }
+
   // Rearrage all images
   // @param centerIndex: indicate which image stays in the center
   rearrange(centerIndex) {
@@ -70,7 +101,8 @@ class AppComponent extends React.Component {
     // give position to image in the center position
     let imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex, 1);
     imgsArrangeCenterArr[0] = {
-      pos: this.Constant.centerPos
+      pos: this.Constant.centerPos,
+      rotate: 0
     }
     // give position to image in the top section
     let topImgNum = Math.floor(Math.random() * 2);
@@ -85,6 +117,7 @@ class AppComponent extends React.Component {
       // }
       value.pos.top = getRangeRandom(this.Constant.vPosRange.topY[0], this.Constant.vPosRange.topY[1]);
       value.pos.left = getRangeRandom(this.Constant.vPosRange.x[0], this.Constant.vPosRange.x[1]);
+      value.rotate = get30DegRandom();
     })
     // give position to images in the left and right sections
     let k = imgsArrangeArr.length / 2;
@@ -99,7 +132,8 @@ class AppComponent extends React.Component {
         pos: {
           top: getRangeRandom(this.Constant.hPosRange.y[0], this.Constant.hPosRange.y[1]),
           left: getRangeRandom(hPosRangeLORX[0], hPosRangeLORX[1])
-        }
+        },
+        rotate: get30DegRandom()
       }
     }
     if (imgsArrangeTopArr && imgsArrangeTopArr[0]) {
@@ -155,11 +189,13 @@ class AppComponent extends React.Component {
           pos: {
             left: '0',
             top: '0'
-          }
+          },
+          rotate: 0,
+          isInverse: false
         }
       }
       imgFigures.push(<ImgFigure data={value} ref={'imgFigure' + index}
-                        arrange={this.state.imgsArrangeArr[index]}/>)
+                        arrange={this.state.imgsArrangeArr[index]} inverse={this.inverse(index)}/>)
     });
 
     return (
